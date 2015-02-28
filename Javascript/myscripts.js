a8c908372d1bb79223229b99c68449ff1e5fc553
Sashasun55 <madisonsstern@gmail.com>
2015-02-28 15:27:19 -0500

    women who code - madison stern
    learning how to code using html, css, javascript

---
+++ Javascript/myscripts.js
@@ -0,0 +1,9 @@
+//DOM = Document Object Model
+//Window.onload=function(){myScript};
+
+window.onload=function(){
+  var but = document.getElementById("but");
+but.onclick=function(){
+  alert("clicked");
+};
+};
